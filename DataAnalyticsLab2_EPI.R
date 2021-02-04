e26d0e4a926fce197cc57210dbdb4817e8b93e0e
Devanshoo <61462149+Devanshoo@users.noreply.github.com>
2021-02-04 10:53:38 -0500

    Update DataAnalyticsLab2_EPI.R

--- DataAnalyticsLab2_EPI.R
+++ DataAnalyticsLab2_EPI.R
@@ -12,6 +12,8 @@ attach(EPI_data)
 ??attach
 fix(EPI_data)
 
+#EXERCISE 1: EXPLORING THE DISTRIBUTION
+
 summary(EPI_data)
 head(EPI_data)
 
@@ -35,3 +37,59 @@ hist(EPI, seq(30.,95.,1.0),probability = TRUE)
 lines(density(EPI,na.rm=TRUE,bw=1.))
 
 rug(EPI)
+
+# Fitting a distribution beyong histograms
+
+??ecdf
+plot(ecdf(EPI), do.points=FALSE, verticals=TRUE) 
+
+#Quantile-Quantile
+??qqnorm
+par(pty="s")
+qqnorm(EPI)
+qqline(EPI)
+
+x <- seq(30,95,1)
+qqplot(qt(ppoints(250), df = 5), x, xlab = "Q-Q plot for t dsn")
+qqline(x)
+
+head(DALY)
+View(DALY)
+summary(DALY)
+plot(ecdf(DALY),do.points=FALSE,verticals = TRUE)
+
+qqnorm(DALY)
+qqline(EPI)
+x<-seq(50,70,0.5)
+
+qqplot(qt(ppoints(250), df = 5), x, xlab = "Q-Q plot for DALY")
+qqline(x)
+
+boxplot(EPI,DALY)
+qqplot(EPI,DALY)
+
+
+#EXCERCISE 2: FILTERING POPULATIONS
+View(EPILand)
+head(EPILand)
+
+EPILand<-EPI[!Landlock]
+land_d <- EPILand[!is.na(EPILand)]
+hist(land_d)
+hist(land_d, seq(30., 95., 1.0), prob=TRUE)
+
+tf <- is.na(land_d)
+E <- land_d[!tf]
+E
+
+qqline(land_d)
+x<-seq(50,70,0.5)
+
+qqplot(qt(ppoints(250), df = 5), x, xlab = "Q-Q plot for land")
+qqline(x)
+
+boxplot(EPI,land_d)
+qqplot(EPI,land_d)
+
+
+
